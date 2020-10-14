5f8b1db5082f2fbb2e48075b1b7b6531243c307f
Jamil Ohayia <jamil.ohayia@columbia.edu>
2020-10-14 16:17:08 -0400

    added some comments

---
+++ routes/apiRoutes.js
@@ -0,0 +1,48 @@
+const db = require("../db/db.json");
+const fs = require("fs");
+const path = require('path');
+const util = require("util");
+
+console.log('Current Dir: ' + __dirname);
+
+module.exports = function (app) {
+  app.get("/api/notes", function (req, res) {
+    // res.json(db);
+    res.sendFile(path.join(__dirname, "../db/db.json"));
+  });
+
+  app.post("/api/notes", function (req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    console.log(savedNotes);
+    let newNote = req.body;
+    //using note position in arr length # as unique id
+    // let uniqueID = (savedNotes.length).toString();
+    let uniqueID = (savedNotes.length);
+    console.log('setting newNotes id to ' + uniqueID);
+    newNote.id = uniqueID;
+    savedNotes.push(newNote);
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+    console.log("Note saved to db.json: ", newNote);
+    res.json(savedNotes);
+  })
+
+  app.delete("/api/notes/:id", function (req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let noteID = req.params.id;
+    let newID = 0;
+    console.log(`Deleting note with ID ${noteID}`);
+    savedNotes = savedNotes.filter(currentNote => {
+      return currentNote.id != noteID;
+    })
+
+    for (currentNote of savedNotes) {
+      currentNote.id = newID.toString();
+      newID++;
+    }
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+    res.json(savedNotes);
+  })
+
+}
---
+++ routes/htmlRoutes.js
@@ -0,0 +1,16 @@
+// This code is used to generate the correct file path for the HTML file 
+
+var path = require("path");
+
+module.exports = function(app) {
+ 
+
+  app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+  });
+
+ 
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+};
